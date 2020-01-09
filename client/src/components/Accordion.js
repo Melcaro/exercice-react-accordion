663af4bc0b0ccbcacc5effbbecc581e63935a555
Mel <melanie.caro78@gmail.com>
2020-01-09 17:26:38 +0100

    start app

---
+++ client/src/components/Accordion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Accordion extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Make an Accordion with CSS Animations</h1>
+      </div>
+    );
+  }
+}
